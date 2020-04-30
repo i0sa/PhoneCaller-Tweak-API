ee12e70416159e4d6d0c8100e2335b819c1a0687
i0sa <me@i0sa.com>
2020-04-30 19:32:25 +0200

    Create PhoneCallerHelper.h

---
+++ Headers/PhoneCallerHelper.h
@@ -0,0 +1,36 @@
+//
+//  PhoneCallerHelper.h
+//  
+//
+//  Created by Osama on 4/30/20.
+//  Copyright © 2020 Osama Gamal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface PhoneCallerHelper : NSObject
+
+typedef void(^postRequestBlock)(PCCaller *details);
+typedef void(^searchingBlock)(PCEngine *engine);
+typedef void(^searchCompleteBlock)(PCEngine *engine);
+typedef void(^searchItemBlock)(PCSearchItem *searchItem);
+
+
+@property (nonatomic, copy) postRequestBlock completed;
+@property (nonatomic, copy) searchingBlock searching;
+@property (nonatomic, copy) searchItemBlock searchItem;
+@property (nonatomic, copy) searchCompleteBlock searchComplete;
+
+
+//MARK: Normal Search With Mobile
+-(void)searchForNumber:(NSString *)number
+             completed:(postRequestBlock)completed
+             searching:(searchingBlock)searching
+            searchItem:(searchItemBlock)searchItem
+        searchComplete:(searchCompleteBlock)searchComplete;
+
+@end
+
+
