c011017126be5ff0da08d85d5fbd193347a85f2e
i0sa <me@i0sa.com>
2020-04-30 19:35:53 +0200

    Create PCCaller.h

---
+++ Headers/PCCaller.h
@@ -0,0 +1,20 @@
+//
+//  PCCaller.h
+//  testBaseee
+//
+//  Created by Osama on 8/21/19.
+//  Copyright © 2019 Osama Gamal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PCCaller : NSObject
+@property (strong, nonatomic) NSString *number;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *image;
+@property (strong, nonatomic) NSString *date;
+@end
+
+NS_ASSUME_NONNULL_END
