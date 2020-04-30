9c11f40d88ea52fc4fa2ac0228c9ed1c900b899d
i0sa <me@i0sa.com>
2020-04-30 19:33:46 +0200

    Create PCEngine.h

---
+++ Headers/PCEngine.h
@@ -0,0 +1,14 @@
+//
+//  PCEngine.h
+//  PhoneCaller3Xcode
+//
+//  Created by Osama on 3/26/20.
+//  Copyright © 2020 Osama Gamal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCEngine : NSObject
+@property (nonatomic, strong) NSString *identifier;
+@end
+
